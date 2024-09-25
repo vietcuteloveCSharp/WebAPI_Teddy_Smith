22ec05ff0c15094875e2c8220017ad31892b0b40
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-26 00:31:13 +0700

    full-repo-get

---
+++ WebAPI_Teddy_Smith/Repository/ReviewRepository.cs
@@ -0,0 +1,34 @@
+﻿using WebAPI_Teddy_Smith.Data;
+using WebAPI_Teddy_Smith.Interface;
+using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Repository
+{
+    public class ReviewRepository : IReviewRepository
+    {   private readonly DataContext _context;
+        
+        public ReviewRepository( DataContext context)
+        {
+            this._context = context;
+        }
+        public Review GetReview(int reviewId)
+        {
+            return _context.Reviews.Where(c => c.Id == reviewId).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviews()
+        {
+            return _context.Reviews.ToList();
+        }
+
+        public ICollection<Review> GetReviewsOfAPokemon(int pokeId)
+        {
+           return _context.Reviews.Where(c=>c.Pokemon.Id==pokeId).ToList();
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            return _context.Reviews.Any(c=>c.Id == reviewId);
+        }
+    }
+}
---
+++ WebAPI_Teddy_Smith/Repository/ReviewerRepository.cs
@@ -0,0 +1,33 @@
+﻿using WebAPI_Teddy_Smith.Data;
+using WebAPI_Teddy_Smith.Interface;
+using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {   private readonly DataContext _context;
+        public ReviewerRepository(DataContext context)
+        {
+            this._context = context;
+        }
+        public ICollection<Review> GetReviewByReviewer(int reviewerId)
+        {
+           return _context.Reviews.Where(c=>c.Reviewer.Id==reviewerId).ToList();
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(c => c.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(c => c.Id == reviewerId);
+        }
+    }
+}
