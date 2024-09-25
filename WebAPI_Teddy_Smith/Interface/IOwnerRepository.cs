0a023b126b0ba75506f9b3bafdf47af60c2aa2c6
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-25 23:38:06 +0700

    Update-interface

---
+++ WebAPI_Teddy_Smith/Interface/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Interface
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int id);
+        ICollection<Owner> GetOwnerOfPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
