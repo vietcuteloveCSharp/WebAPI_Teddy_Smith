d2c7ed4a29e51f1025d03a91622d36a3b6fc4bec
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-25 23:28:52 +0700

    update full migrations

--- WebAPI_Teddy_Smith/Migrations/20240925161357_edit-table-owner.Designer.cs
+++ WebAPI_Teddy_Smith/Migrations/20240925161357_edit-table-owner.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI_Teddy_Smith.Data;
 
@@ -11,9 +12,11 @@ using WebAPI_Teddy_Smith.Data;
 namespace WebAPI_Teddy_Smith.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240925161357_edit-table-owner")]
+    partial class edittableowner
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAPI_Teddy_Smith/Migrations/20240925161357_edit-table-owner.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_Teddy_Smith.Migrations
+{
+    /// <inheritdoc />
+    public partial class edittableowner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Owners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                 name: "Name",
+                 table: "Owners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
