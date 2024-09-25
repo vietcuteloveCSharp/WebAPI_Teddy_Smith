dc9822e1470752b58fbf5c496e0d18b2bb454269
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-26 00:29:26 +0700

    Full service interface

---
+++ WebAPI_Teddy_Smith/Interface/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Interface
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExists(int reviewId);   
+
+    }
+}
---
+++ WebAPI_Teddy_Smith/Interface/IReviewerRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Interface
+{
+    public interface IReviewerRepository
+
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int reviewerId);
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        bool ReviewerExists(int reviewerId);
+    }
+}
