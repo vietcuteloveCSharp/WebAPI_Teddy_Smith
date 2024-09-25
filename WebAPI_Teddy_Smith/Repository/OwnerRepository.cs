659b8d1cb6dd501e2e65ec24eca9faad84329712
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-25 23:39:04 +0700

    update-repo

---
+++ WebAPI_Teddy_Smith/Repository/OwnerRepository.cs
@@ -0,0 +1,39 @@
+﻿using WebAPI_Teddy_Smith.Data;
+using WebAPI_Teddy_Smith.Interface;
+using WebAPI_Teddy_Smith.Models;
+
+namespace WebAPI_Teddy_Smith.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+        public OwnerRepository(DataContext context)
+        {
+            this._context= context; 
+        }
+        public Owner GetOwner(int id)
+        {
+            return _context.Owners.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfPokemon(int pokeId)
+        {
+            return _context.PokemonOwners.Where(c=>c.Pokemon.Id == pokeId).Select(c=>c.Owner).ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _context.PokemonOwners.Where(c => c.Owner.Id == ownerId).Select(c => c.Pokemon).ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(c => c.Id == ownerId);
+        }
+    }
+}
