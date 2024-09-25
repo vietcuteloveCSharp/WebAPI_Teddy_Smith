f64a8d44c2f9b1775a1c7b41a110c1005148ecf8
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2024-09-25 23:35:11 +0700

    Update-DTO

---
+++ WebAPI_Teddy_Smith/DTO/OwnerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_Teddy_Smith.DTO
+{
+    public class OwnerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
